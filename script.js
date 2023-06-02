d55625e3230e9373a8395c9e2a168c1a36d8b12a
hlift <venturaguillaume01@gmail.com>
2023-06-02 09:17:30 +0200

    ✨ Add name in search params

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('script initialized')
+
+const params = new URLSearchParams(window.location.search);
+const titleElement = document.getElementById('title');
+const name = params.get('name');
+titleElement.innerHTML = `Hello ${name}`;
